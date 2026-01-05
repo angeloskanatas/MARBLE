17fa22bb2c8958e08472d922331a81c23c60ef08
Angelos-Nikolaos Kanatas <akanatas@hotmail.com>
2026-01-05 13:46:59 +0100

    musicgen_sonics-real extraction config

---
+++ scripts/extract.MusicGen.sonics-real.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=extract_musicgen-large_sonics-real
+#SBATCH --partition=medium
+#SBATCH --nodes=1
+#SBATCH --exclude=node025
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=24
+#SBATCH --gres=gpu:quadro:1
+#SBATCH --mem=128G
+#SBATCH --time=04:00:00
+#SBATCH --chdir=/home/akanatas/projects/layerbylayer/MARBLE
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+
+module load Miniconda3/4.9.2
+eval "$(conda shell.bash hook)"
+conda activate layerbylayer
+
+module load CUDA/11.4.3
+
+python cli.py test -c configs/extract.MusicGen.sonics-real.yaml
